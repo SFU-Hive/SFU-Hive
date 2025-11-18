d5ee491e6da320e354cd4572af8ce6242c30d7ce
Dylan Young <dylanyoung461@gmail.com>
2025-11-17 21:09:05 -0800

    adds recent files grid to dashboard

---
+++ app/src/main/java/com/project362/sfuhive/Dashboard/RecentFilesAdaptar.kt
@@ -0,0 +1,53 @@
+package com.project362.sfuhive.Dashboard
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.content.Context
+import android.view.LayoutInflater
+import android.widget.TextView
+import com.project362.sfuhive.Dashboard.DashboardFragment.RecentFile
+import com.project362.sfuhive.R
+
+
+class RecentFilesAdaptar(
+    private val context: Context,
+    private val recentFiles: List<RecentFile>
+) : BaseAdapter() {
+
+    private val inflator: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+
+    override fun getCount(): Int {
+        return recentFiles.size
+    }
+
+    override fun getItem(pos: Int): Any? {
+        return recentFiles[pos]
+    }
+
+    override fun getItemId(pos: Int): Long {
+        return pos.toLong()
+    }
+
+    override fun getView(
+        pos: Int,
+        convertView: View?,
+        parent: ViewGroup?
+    ): View? {
+        val view : View
+        if(convertView == null){
+            view = inflator.inflate(R.layout.item_recent_file, parent, false)
+        } else {
+            view = convertView
+        }
+
+        val recentFile = getItem(pos) as RecentFile
+
+        val fileName = view.findViewById<TextView>(R.id.file_name)
+        fileName.text = recentFile.fileName
+
+        return view
+    }
+
+}
