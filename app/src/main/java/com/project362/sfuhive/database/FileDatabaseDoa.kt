e1a04da67fa176cfa123e6479ee11540ca83dc78
danieldawdaSFU <djd7@sfu.ca>
2025-11-20 17:53:39 -0800

    adds file database and DOA class

---
+++ app/src/main/java/com/project362/sfuhive/database/FileDatabaseDoa.kt
@@ -0,0 +1,24 @@
+package com.project362.sfuhive.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+// adapted from RoomDatabase demo
+@Dao
+interface FileDatabaseDoa {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFile(file: File)
+
+    @Query("DELETE FROM file_table")
+    suspend fun deleteAll()
+
+    @Query("SELECT * FROM file_table")
+    fun getAllFiles(): Flow<List<File>>
+
+    @Query("SELECT * FROM file_table WHERE fileId = :key LIMIT 1")
+    suspend fun getFile(key: Long): File?
+}
