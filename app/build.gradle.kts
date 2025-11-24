a44d42fda5ea114e290078171969644bc58f47d5
Miro-R <miro.rogers@gmail.com>
2025-11-23 17:19:11 -0800

    got 'badge unlocked' dialog working and 'reset badge' button added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.sqlite.ktx)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
